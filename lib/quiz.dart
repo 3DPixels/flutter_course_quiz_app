28040796b036311a7b7f0978fb8cf09e1287933d
Ammar <ammar.salim2018@gmail.com>
2023-11-28 11:38:26 +0200

    project complete

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -23,10 +24,16 @@ class _QuizState extends State<Quiz> {
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
     if (selectedAnswers.length == questions.length) {
-      selectedAnswers = [];
-      activeScreen = 'start-screen';
+      activeScreen = 'results-screen';
       setState(() {});
     }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
   }
 
   @override
@@ -37,6 +44,10 @@ class _QuizState extends State<Quiz> {
       screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
     }
 
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers, onRestart: restartQuiz,);
+    }
     return MaterialApp(
       home: Scaffold(
         body: Container(
---
+++ lib/results_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary/questions_summary.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen(
+      {super.key, required this.chosenAnswers, required this.onRestart});
+
+  final List<String> chosenAnswers;
+  final void Function() onRestart;
+
+  //map value type is object because it has different types
+  List<Map<String, Object>> getSummaryData() {
+    print('called');
+    final List<Map<String, Object>> summary = [];
+
+    //as we said before you can add to a final list
+    for (var i = 0; i < questions.length; i++) {
+      //we use curly braces in add to add a map
+      print(questions[i].text);
+      print(questions[i].answers[0]);
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'You Answered $numCorrectQuestions out of $numTotalQuestions questions Correctly',
+            style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 230, 200, 253),
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 30),
+          QuestionsSummary(summaryData),
+          const SizedBox(height: 30),
+          TextButton.icon(
+            onPressed: onRestart,
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.refresh),
+            label: const Text('Restart Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
